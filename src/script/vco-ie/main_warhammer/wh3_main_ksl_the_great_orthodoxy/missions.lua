fee6996f37016c152102ba766e42eb9acfd18844
mike141@live.com <mike141@live.com>
2022-09-21 20:51:35 +0100

    added great orthodoxy's mission file and tested working

---
+++ src/script/vco-ie/main_warhammer/wh3_main_ksl_the_great_orthodoxy/missions.lua
@@ -0,0 +1,125 @@
+local missions = {
+	[[
+ 		mission
+		{
+			victory_type vco_victory_type_ksl_ort_1_there_can_only_be_one;
+			key wh_main_short_victory;
+			issuer CLAN_ELDERS;
+			primary_objectives_and_payload
+			{
+
+				objective
+				{
+					override_text mission_text_text_vco_ksl_ort_1_there_can_only_be_one;
+					type SCRIPTED;
+					script_key vco_dummy;
+				}
+
+				payload
+				{
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_ksl_ort_1_there_can_only_be_one_1;
+						turns 0;
+					}
+
+					effect_bundle
+					{
+						bundle_key vco_campaign_victory_dummy;
+						turns 0;
+					}
+
+					game_victory;
+				}
+			}
+		}
+	]],
+	[[
+ 		mission
+		{
+			victory_type vco_victory_type_ksl_ort_2_the_last_bastion;
+			key wh_main_short_victory;
+			issuer CLAN_ELDERS;
+			primary_objectives_and_payload
+			{
+				objective
+				{
+					override_text mission_text_text_vco_ksl_ort_2_the_last_bastion;
+					type SCRIPTED;
+					script_key vco_dummy;
+				}
+
+				payload
+				{
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_ksl_ort_2_the_last_bastion_1;
+						turns 0;
+					}
+
+					effect_bundle
+					{
+						bundle_key vco_campaign_victory_dummy;
+						turns 0;
+					}
+
+					game_victory;
+				}
+			}
+		}
+	]],
+	[[
+ 		mission
+		{
+			victory_type vco_victory_type_ksl_ort_3_wololo;
+			key wh_main_short_victory;
+			issuer CLAN_ELDERS;
+			primary_objectives_and_payload
+			{
+				objective
+				{
+					override_text mission_text_text_vco_ksl_ort_3_wololo;
+					type SCRIPTED;
+					script_key vco_dummy;
+				}
+
+				payload
+				{
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_ksl_ort_3_wololo_1;
+						turns 0;
+					}
+				
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_ksl_ort_3_wololo_2;
+						turns 0;
+					}
+				
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_ksl_ort_3_wololo_3;
+						turns 0;
+					}
+				
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_ksl_ort_3_wololo_4;
+						turns 0;
+					}
+
+					effect_bundle
+					{
+						bundle_key vco_campaign_victory_dummy;
+						turns 0;
+					}
+
+					game_victory;
+				}
+			}
+		}
+	]]
+};
+
+return missions;
