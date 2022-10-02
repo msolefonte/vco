8b279a4d79f75b791db6c83b79758282f60835e2
mike141@live.com <mike141@live.com>
2022-10-02 16:56:12 +0100

    Updated is_character_level_x function and applied in Khorne listener

--- src/script/campaign/mod/vco-listeners.lua
+++ src/script/campaign/mod/vco-listeners.lua
@@ -28,7 +28,7 @@ local function is_faction_vassal_or_destroyed(player_faction, target_faction_key
 end
 
 local function is_character_level_x(character, level)
-  return true;
+  return character:rank() >= level
 end
 
 local function is_faction_under_your_control(player_faction, target_faction_key, consider_military_allies)
@@ -291,15 +291,13 @@ local function add_listeners()
 
 	vco:log("- Khorne listeners");
 	core:add_listener(
-		"vco_kho_skarbrand_level_check",
+		"vco_kho_skarbrand_level_40_check",
 		"CharacterRankUp",
 		function(context)
-			local faction = context:faction();
-			return faction:is_human() and faction:name() == "wh3_main_kho_exiles_of_khorne"
+			return context:faction():is_human() and context:character():unit_key() == "wh3_main_kho_skarbrand"
 		end,
 		function(context)
-			-- TODO: replace this with check using is_character_level_x(context:character())
-			check_vco_brt_bordeleaux_alberic_vow(context:character());
+			is_character_level_x(context:character(), 40);
 		end,
 		true
 	);
