fa3190750adf8edd8ae6a899e2c88315ccfe70c0
mike141@live.com <mike141@live.com>
2022-10-19 21:31:49 +0100

    Initial World Walkers foundation - all three routes display

---
+++ src/script/vco-ie/main_warhammer/wh_dlc08_nor_norsca/missions.lua
@@ -0,0 +1,159 @@
+local missions = {
+	[[
+ 		mission
+		{
+			victory_type vco_victory_type_nor_wul_1_ragnarok;
+			key wh_main_short_victory;
+			issuer CLAN_ELDERS;
+			primary_objectives_and_payload
+			{
+				objective
+				{
+					override_text mission_text_text_vco_nor_wul_1_ragnarok;
+					type SCRIPTED;
+					script_key vco_dummy;
+				}
+
+				objective
+				{
+					type CONSTRUCT_N_BUILDINGS_INCLUDING;
+					faction wh2_main_skv_clan_moulder;
+					total 1;
+
+					building_level wh2_main_special_hellpit_pits;
+				}
+
+				payload
+				{
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_nor_wul_1_ragnarok_1;
+						turns 0;
+					}
+				
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_nor_wul_1_ragnarok_2;
+						turns 0;
+					}
+
+					effect_bundle
+					{
+						bundle_key vco_campaign_victory_dummy;
+						turns 0;
+					}
+
+					game_victory;
+				}
+			}
+		}
+	]],
+	[[
+ 		mission
+		{
+			victory_type vco_victory_type_nor_wul_2_the_world_walker;
+			key wh_main_short_victory;
+			issuer MUFFIN_MAN;
+			primary_objectives_and_payload
+			{
+				objective
+				{
+					override_text mission_text_text_vco_nor_wul_2_the_world_walker;
+					type SCRIPTED;
+					script_key vco_dummy;
+				}
+
+				objective
+				{
+					type DESTROY_FACTION;
+
+					faction wh3_main_ksl_the_ice_court;
+					faction wh3_main_ksl_the_great_orthodoxy;
+				}
+			
+				objective
+				{
+					type RAZE_OR_SACK_N_DIFFERENT_SETTLEMENTS_INCLUDING;
+					total 4;
+				
+					region wh3_main_combi_region_altdorf;
+					region wh3_main_combi_region_kislev;
+					region wh3_main_combi_region_middenheim;
+					region wh3_main_combi_region_brass_keep;
+					region wh3_main_combi_region_gryphon_wood;
+					region wh3_main_combi_region_castle_drakenhof;
+					region wh3_main_combi_region_karak_kadrin;
+				}
+
+				payload
+				{
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_nor_wul_2_the_world_walker;
+						turns 0;
+					}
+
+					effect_bundle
+					{
+						bundle_key vco_campaign_victory_dummy;
+						turns 0;
+					}
+
+					game_victory;
+				}
+			}
+		}
+	]],
+	[[
+ 		mission
+		{
+			victory_type vco_victory_type_nor_wul_3_wheres_my_palico;
+			key wh_main_short_victory;
+			issuer CLAN_ELDERS;
+			primary_objectives_and_payload
+			{
+				objective
+				{
+					override_text mission_text_text_vco_nor_wul_3_wheres_my_palico;
+					type SCRIPTED;
+					script_key vco_dummy;
+				}
+
+				objective
+				{
+					type DESTROY_FACTION;
+
+					faction wh_main_dwf_kraka_drak;
+					faction wh_dlc08_nor_goromadny_tribe;
+
+					confederation_valid;
+				}
+
+				payload
+				{
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_nor_wul_3_wheres_my_palico_1;
+						turns 0;
+					}
+				
+					effect_bundle
+					{
+						bundle_key vco_victory_payload_nor_wul_3_wheres_my_palico_2;
+						turns 0;
+					}
+
+					effect_bundle
+					{
+						bundle_key vco_campaign_victory_dummy;
+						turns 0;
+					}
+
+					game_victory;
+				}
+			}
+		}
+	]]
+};
+
+return missions;
