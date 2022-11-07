03c6f6782bf228a491d74abbe2880f734191a799
Marc Solé <msolefonte@gmail.com>
2022-11-07 13:33:45 +0100

    Update 5.0.0-beta.7.HOTFIX-1 (#243) (#245)
    - Fixed a bug that allowed AI factions to recruit unlockable units
    - Improved flavour and replaced icons
    
    Co-authored-by: Marc Solé <msolefonte@gmail.com>
    Co-authored-by: Mjb141 <46346590+Mjb141@users.noreply.github.com>

--- src/script/campaign/mod/vco-listeners-nor.lua
+++ src/script/campaign/mod/vco-listeners-nor.lua
@@ -96,7 +96,6 @@ local function add_listeners()
 		"FactionTurnStart",
 		function(context)
 			return cm:model():turn_number() == 1 and
-				context:faction():is_human() and
 				context:faction():name() == FACTION_WUL_KEY;
 		end,
 		wul_lock_units,
